50f5f261e35c1bcdfe755b729fcaf1bebccf8d49
chelseafood <44086164+chelseafood@users.noreply.github.com>
2019-08-30 14:07:26 +0800

    Update TestHtmlHelperLabel.cs

--- TestHtmlHelperLabel.cs
+++ TestHtmlHelperLabel.cs
@@ -5,11 +5,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace iSing_Company_Test1_MVCMiniApp
+namespace MVCMiniApp
 {
     public class TestHtmlHelperLabel
     {
-        public static MvcHtmlString TaxEditorFor(string text)
+        public static MvcHtmlString TestEditorFor(string text)
         {
             var tagBuilder = new TagBuilder("label");
             tagBuilder.AddCssClass("myLabel");
